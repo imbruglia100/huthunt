f2454fa69c93d9abc7c465930572cfdda3b01b93
frank <imbruglia100@gmail.com>
2024-06-13 19:10:55 -0400

    add review stars function

--- frontend/src/components/ReviewModal/ReviewModal.jsx
+++ frontend/src/components/ReviewModal/ReviewModal.jsx
@@ -1,74 +1,71 @@
 /** @format */
 
-import { useState } from "react";
-// import { FaStar } from "react-icons/fa";
-import './ReviewModal.css'
+import { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./ReviewModal.css";
 import { useDispatch } from "react-redux";
 import { createAReviewWithId, getReviewsBySpotById } from "../../store/reviews";
 import { useModal } from "../../context/Modal";
 import { getSpotById } from "../../store/spots";
 
-const AddReviewModal = ({spotId}) => {
-  const {closeModal} = useModal()
+const AddReviewModal = ({ spotId }) => {
+  const { closeModal } = useModal();
   const [review, setReview] = useState({
-    review: '',
-    stars: 5
-  })
-const dispatch = useDispatch()
-
-  //   const starContainer= document.getElementById("star-rating-container")
-
-  //   const starArr = Array.from(starContainer.childNodes)
-
-  //   starContainer.addEventListener('mouseover', (e) => {
-  //   e.stopPropagation()
-  //   const currStar = e.target
-  //   const currIndex = starArr.indexOf(currStar)
-  //   if(starArr.length > 0 && starArr.includes(e.target)){
-  //     starArr.forEach((star, i) => {
-  //       if(i <= currIndex){
-  //         star.classList.add('gold')
-  //       }
-  //     })
-  //   }
-  // })
-
-  const handleChange = e => {
-    e.preventDefault();
-    setReview(prev => {return{...prev, stars: +e.target.value}})
-  }
-  const handleSubmit = async () => {
-
-    dispatch(createAReviewWithId(spotId, review))
-    dispatch(getReviewsBySpotById(spotId))
-    dispatch(getSpotById(spotId))
-    closeModal()
+    review: "",
+    stars: 1,
+  });
+  const dispatch = useDispatch();
+  const stars = document.querySelectorAll(".star");
+  const handleRating = (val) => {
+    setReview((prev) => ({ ...prev, stars: val }));
   };
+
+  useEffect(() => {
+
+    stars.forEach((star) => {
+      console.log('adding event listener', star)
+      star.addEventListener("click", handleRating);
+    });
+
+  },[])
+  const handleSubmit = async () => {
+    dispatch(createAReviewWithId(spotId, review));
+    dispatch(getReviewsBySpotById(spotId));
+    dispatch(getSpotById(spotId));
+    closeModal();
+  };
+
   return (
     <form className='modalForm' onSubmit={handleSubmit}>
       <h1>How was your stay?</h1>
       <div className='input-container'>
-        <textarea style={{height: "100px"}} onChange={(e) => setReview(prev=> {return {...prev, review:e.target.value}})} value={review.review} placeholder='Leave your review here...' />
+        <textarea
+          style={{ height: "100px" }}
+          onChange={(e) =>
+            setReview((prev) => {
+              return { ...prev, review: e.target.value };
+            })
+          }
+          value={review.review}
+          placeholder='Leave your review here...'
+        />
       </div>
       <div className='input-container'>
-        {/* <div id="star-rating-container">
-          <FaStar id='0'/>
-          <FaStar id='1'/>
-          <FaStar id='2'/>
-          <FaStar id='3'/>
-          <FaStar id='4'/>
-        </div> */}
-        <label>Star Rating</label>
-          <select onChange={handleChange} defaultValue={review.stars}>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-          </select>
+        <div id='star-rating-container'>
+          {[1, 2, 3, 4, 5].map((value) => (
+            <FaStar
+              key={value}
+              className={`star ${value <= review.stars ? "gold" : ""}`}
+              data-value={value}
+              onClick={() => handleRating(value)}
+            />
+          ))}
+        </div>
       </div>
 
-      <button disabled={review.review.length < 10} className='primary-btn'>Submit your review</button>
+      <button disabled={review.review.length < 10} className='primary-btn'>
+        Submit your review
+      </button>
     </form>
   );
 };
